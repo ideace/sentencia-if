1d9f9eb3a497375502072f149460d6041a4448a5
ivand <ivandariorap1@outlook.es>
2022-07-07 20:07:14 -0500

    sentencia if

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //condicional if
+        int numeroIf = -5;
+
+        if (numeroIf == 0) {
+            System.out.println("el numero es cero");
+        } else if (numeroIf <0) {
+            System.out.println("el numero es negativo");
+        }
+        if (numeroIf >0) {
+            System.out.println("el numero es positivo");
+        }
+    }
+
+
+}
